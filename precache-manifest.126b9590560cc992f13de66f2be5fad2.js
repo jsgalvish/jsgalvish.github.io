4eef4f770b068314c3cc91100ef8c4930418b93b
JS Galvis H <jsgalvish@unal.edu.co>
2020-03-10 12:58:26 -0500

    Updates

---
+++ precache-manifest.126b9590560cc992f13de66f2be5fad2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3128157f95f7de1be1cbd7873aee68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5504712292b11850b8c",
+    "url": "/static/css/main.97cac852.chunk.css"
+  },
+  {
+    "revision": "c18714de18679160370a",
+    "url": "/static/js/2.6b9c89e4.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.6b9c89e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5504712292b11850b8c",
+    "url": "/static/js/main.4be47b39.chunk.js"
+  },
+  {
+    "revision": "3519d40d78d5be199fad",
+    "url": "/static/js/runtime-main.1f27edf0.js"
+  }
+]);
